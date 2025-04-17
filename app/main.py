1b4f52bb64a20334cedf1d2bf6d49dcf0daff47c
Prathmesh Bhatt <ppbhatt500@gmail.com>
2025-04-16 18:14:50 -0700

    small changes

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
 
 @app.get("/")
 async def root():
-    return "Hello World"
+    return "Hello World!!!!!!!"
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
